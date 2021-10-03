3a2a109fe117456d294eb16bb3a126b29f3aec74
Cristiane <crisdelapedra@gmail.com>
2021-10-03 13:05:00 -0300

    Fundamentos javascript

---
+++ Formatacao_Valor.js
@@ -0,0 +1,22 @@
+function ganhoPorHora(salario, horasTrabalhadasNoMes) {
+
+  const salarioHora = (salario / horasTrabalhadasNoMes); 
+
+  const total = salarioHora.toFixed(2);
+
+  return total;
+
+};
+
+function ganhoPorHoraMoeda(salario, horasTrabalhadasNoMes) {
+
+  const salarioHora = (salario / horasTrabalhadasNoMes); 
+
+  const formatado = salarioHora.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+  return formatado;
+
+}
+
+console.log(ganhoPorHoraMoeda(3000,176)); 
+
---
+++ String.js
@@ -0,0 +1,5 @@
+const cidade = "belo horizonte";
+const cidade2 = "Belo Horizonte";
+
+console.log(cidade.toLowerCase() === cidade2.toLowerCase());
+console.log(cidade.length);
